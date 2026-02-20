8c51f32b3850adc3784b943bec1f95f1eb748186
CodeWithAbdullah-007 <mabdullahanwar85@gmail.com>
2026-02-20 16:58:02 +0500

    Done

---
+++ script.js
@@ -0,0 +1,84 @@
+// CHAPTER_79
+
+// let newWindow = window.open();
+// console.log('newWindow:', newWindow);
+
+// =============================================================================================================================================================================
+
+var monkeyWindow = window.open();
+// var windowContent = `<h1>Capuchin monkey</h1><p>The word capuchin derives from a
+// group of friars<br>named the Order of Friars Minor Capuchin who wear<br>brown
+// robes with large hoods covering their heads.</p`;
+// monkeyWindow.document.write(windowContent)
+
+// =============================================================================================================================================================================
+
+// monkeyWindow.location.assign("https://www.youtube.com/");
+
+// =============================================================================================================================================================================
+
+// var monkeyWindow = window.open("https://www.youtube.com/");
+// console.log('monkeyWindow:', monkeyWindow);
+
+// =============================================================================================================================================================================
+
+// var monkeyWindow = window.open("monk.html", "win1", "width=420,height=380");
+// console.log('monkeyWindow:', monkeyWindow);
+
+// =============================================================================================================================================================================
+
+// function checkForPopBlocker() {
+//     var testPop = window.open("", "", "width=100,height=100");
+//     if (testPop === null) {
+//         alert("Please disable your popup blocker.");
+//     }
+//     testPop.close();
+// }
+
+// =============================================================================================================================================================================
+
+// function checkForSelection() {
+//     if (document.getElementById("states").selectedIndex === 0) {
+//         alert("Please select a state.");
+//         return false;
+//     }
+// }
+
+// =============================================================================================================================================================================
+
+// let num = 10;
+// try{
+//     console.log('num:', num.toLowerCase());
+// }
+// catch(mast){
+//     console.log('error comes:', mast);
+// }
+
+// =============================================================================================================================================================================
+
+function checkPassword() {
+    try {
+        var pass = document.getElementById("f1").value;
+        if (pass.length < 8) {
+            throw "Please enter at least 8 characters.";
+        }
+        if (pass.indexOf(" ") !== -1) {
+            throw "No spaces in the password, please.";
+        }
+        var numberSomewhere = false;
+        for (var i = 0; i < pass.length; i++) {
+            if (isNaN(pass(i, i + 1)) === false) {
+                numberSomewhere = true;
+                break;
+            }
+        }
+        if (numberSomewhere === false) {
+            throw "Include at least 1 number.";
+        }
+    }
+    catch (err) {
+        alert(err);
+    }
+}
+
+// =============================================================================================================================================================================
